886dfb918843549711d68630cb1995160933ba99
Madan <mksiwakoti@gmail.com>
2018-03-27 22:30:44 -0500

    project setup

---
+++ src/main/java/com/madan/activemq/ActivemqTestApplication.java
@@ -0,0 +1,12 @@
+package com.madan.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqTestApplication.class, args);
+	}
+}
